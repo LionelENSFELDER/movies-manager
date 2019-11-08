f9dbf1a9e7a630dd9d54e7bebe47e5f60d5ecff9
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2019-11-08 20:25:54 +0100

    Disable js custom script and update gitignore

--- js/custom_ui.js
+++ js/custom_ui.js
@@ -1,12 +1,12 @@
 //navbar scroll behavior
-window.onscroll = () => {
-	let navbar = document.getElementById("navbar");
-    let brand = document.getElementById("brand");
-	if (window.scrollY <= 50) {
-        navbar.classList.remove("bg-success");
-        //navbar.classList.add("text-success");
-	} else if (window.scrollY > 50) {
-        navbar.classList.add("bg-success");
-        brand.classList.remove("text-success");
-	}
-};
+// window.onscroll = () => {
+// 	let navbar = document.getElementById("navbar");
+//     let brand = document.getElementById("brand");
+// 	if (window.scrollY <= 50) {
+//         navbar.classList.remove("bg-success");
+//         //navbar.classList.add("text-success");
+// 	} else if (window.scrollY > 50) {
+//         navbar.classList.add("bg-success");
+//         brand.classList.remove("text-success");
+// 	}
+// };
